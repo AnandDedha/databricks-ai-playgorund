6861ee067bad199ad38a33f833ec1f0854530b13
Anand Kumar <ananddedha@gmail.com>
2026-06-09 07:20:22 -0400

    Delete notebooks directory

--- notebooks/02_function_definition_demo.py
+++
@@ -1,4 +0,0 @@
-def get_order_status(order_id: str):
-    orders = {"A123": "Delivered", "B456": "In Transit", "C789": "Cancelled"}
-    return {"order_id": order_id, "status": orders.get(order_id, "Not Found")}
-print(get_order_status("B456"))
